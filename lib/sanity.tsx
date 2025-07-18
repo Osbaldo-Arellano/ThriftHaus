dc783a8167e95c4887d65c6038ea25e47fc5065b
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2025-07-18 16:02:12 -0700

    include sanity.tsx

---
+++ lib/sanity.tsx
@@ -0,0 +1,16 @@
+import sanityClient, { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
+
+export const client = createClient({
+  projectId: 'w886j9wx', 
+  dataset: 'production',
+  apiVersion: '2023-01-01',
+  useCdn: true,
+});
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: SanityImageSource) {
+  return builder.image(source);
+}
