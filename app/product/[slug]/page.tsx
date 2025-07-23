ff7ece8ed7d1b902137e3f804bbdf155b8310891
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2025-07-22 20:47:59 -0700

    build fail

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,16 +1,17 @@
-// product/[slug]/page.tsx
 import fetchProductByTitle from '@/lib/fetchProductBySlug'
 import fetchSuggestedProducts from '@/lib/FetchSuggestedProducts'
 import ProductDetails from '@/components/ProductDetails'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
-export const dynamic = 'force-dynamic';  // 👈 this is the fix
+export const dynamic = 'force-dynamic';
 
-export default async function ProductPage({ params }: { params: { slug: string } }) {
-  const decodedTitle = decodeURIComponent(params.slug)
+export default async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params
+  const decodedTitle = decodeURIComponent(slug)
+
   const product = await fetchProductByTitle(decodedTitle)
-  const suggestions = await fetchSuggestedProducts(decodedTitle)
+  const suggestions = await fetchSuggestedProducts(decodedTitle)  // excludes current product
 
   return (
     <>
@@ -21,5 +22,6 @@ export default async function ProductPage({ params }: { params: { slug: string }
       />
       <Footer />
     </>
+
   )
 }
